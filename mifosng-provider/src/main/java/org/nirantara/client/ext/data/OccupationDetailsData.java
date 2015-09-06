4cd104b8f0bbf6e06a69016d06f5f4c671d01068
Srinivas Rao <sre.mifos@gmail.com>
2015-09-06 12:39:37 +0530

    occupationtype label included for client view

--- mifosng-provider/src/main/java/org/nirantara/client/ext/data/OccupationDetailsData.java
+++ mifosng-provider/src/main/java/org/nirantara/client/ext/data/OccupationDetailsData.java
@@ -8,6 +8,7 @@ public class OccupationDetailsData {
 
 	private final Long id;
 	private final Long occupationTypeId;
+	private final String occupationTypeLabel;
 	private final BigDecimal annualRevenue;
 	private final BigDecimal annualExpense;
 	private final BigDecimal annualSurplus;
@@ -20,18 +21,21 @@ public static OccupationDetailsData formOccupationDetailsData(final OccupationDe
 		BigDecimal annualSurplus = occupationDetails.getAnnualSurplus();
 
 		Long occupationTypeId = null;
+		String occupationTypeLabel = null;
 		if(occupationDetails.getOccupationType() != null){
 			occupationTypeId = occupationDetails.getOccupationType().getId();
+			occupationTypeLabel = occupationDetails.getOccupationType().label();
 		}
 		
-		return new OccupationDetailsData(id, occupationTypeId, annualRevenue, annualExpense, annualSurplus);
+		return new OccupationDetailsData(id, occupationTypeId, annualRevenue, annualExpense, annualSurplus, occupationTypeLabel);
 	}
 	
 	private OccupationDetailsData(final Long id, final Long occupationTypeId, 
-			final BigDecimal annualRevenue, final BigDecimal annualExpense,	final BigDecimal annualSurplus) {
+			final BigDecimal annualRevenue, final BigDecimal annualExpense,	final BigDecimal annualSurplus, final String occupationTypeLabel) {
 		
 		this.id = id;
 		this.occupationTypeId = occupationTypeId;
+		this.occupationTypeLabel = occupationTypeLabel;
 		this.annualRevenue = annualRevenue;
 		this.annualExpense = annualExpense;
 		this.annualSurplus = annualSurplus;
@@ -55,6 +59,10 @@ public static OccupationDetailsData formOccupationDetailsData(final OccupationDe
 
 	public Long getOccupationTypeId() {
 		return occupationTypeId;
+	}
+
+	public String getOccupationTypeLabel() {
+		return occupationTypeLabel;
 	}
 	
 	
